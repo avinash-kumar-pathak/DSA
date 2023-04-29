5fd4e9dac300b6c0bc0d21bafc39b0f7a90fe0e7
avinash-kumar-pathak <avinashbxr15@gmail.com>
2023-04-29 22:57:41 +0530

    avi

--- Traverse.java
+++ Traverse.java
@@ -1,4 +1,10 @@
-int A[]= {1,2,3,4,5}
+public class Traverse{
+    public static void main(String[] args) {
+        /**
+         * int A[]= {1,2,3,4,5}
 int i; 
 for(i = 0; i< (len(A[]) - 1); i++)
 Sys.out.print(i);
+         */
+    }
+}

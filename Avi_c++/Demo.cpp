e89e1a7eccbb9b2ec6cb30aabf4763bc1768a414
avinash-kumar-pathak <avinashbxr15@gmail.com>
2023-04-30 20:41:21 +0530

    Avi

---
+++ Avi_c++/Demo.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+int main(){
+int i,j;
+for(i=0;i<=4;i++){
+    for(j=0;j<=4;j++){
+        printf("*\n");
+    }
+}
+}
---
+++ Issues/sort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[5]={23,45,13,25,17};
+    for(int i=0;i<5;i++){
+        cout<<arr[i];
+        cout<<"\n";
+    }
+
+}
+
+
+
+
+
+
+
+

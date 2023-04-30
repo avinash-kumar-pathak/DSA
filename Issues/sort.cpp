968c91c448a6f8113d576d0c282ac25bf1dc9e45
avinash-kumar-pathak <avinashbxr15@gmail.com>
2023-05-01 00:48:37 +0530

    ..

--- Issues/sort.cpp
+++ Issues/sort.cpp
@@ -7,7 +7,7 @@ int main(){
     int arr[size];
         for(int i=0;i<size;i++){
         cin>>arr[i];
-       
+        
         cout<<"\n";
     }
     for(int i=0;i<size;i++){
@@ -15,7 +15,19 @@ int main(){
         cout<<arr[i];
         cout<<"\n";
     }
-        
+    for(int i=0;i<size-1;i++){
+        for(int j=i+1;j<size;j++){
+            if (arr[i]>arr[j])
+            {
+                int Temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=Temp;
+            }
+        }
+    }
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<"\t";
+    }     
 }
 
 
